a2b47a0758bd64d494fd78210e503b6bdf172a47
Víctor Andrés Rojas <victandres98@gmail.com>
2021-04-25 20:16:12 -0500

    Fix Default Theme

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,8 @@ const body = document.body;
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   toggleMode.setAttribute("checked", true);
   body.classList.add("theme--dark");
+} else {
+  body.classList.add("theme--light");
 }
 
 toggleMode.addEventListener("change", function (e) {
