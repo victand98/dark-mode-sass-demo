b207beb2da0f742645cd61fa9fb67d35c7fac0c1
Víctor Andrés Rojas <victandres98@gmail.com>
2021-04-25 20:03:13 -0500

    Adding toggle theme button whit Javascript

---
+++ static/js/index.js
@@ -0,0 +1,12 @@
+const toggleMode = document.querySelector("#toggle-mode");
+const body = document.body;
+
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  toggleMode.setAttribute("checked", true);
+  body.classList.add("theme--dark");
+}
+
+toggleMode.addEventListener("change", function (e) {
+  body.classList.toggle("theme--light");
+  body.classList.toggle("theme--dark");
+});
